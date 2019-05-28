92c097eb5ff5f787ff042e495355c340cbb863a5
Hong seong hyun <shh6155@naver.com>
2019-05-29 08:24:34 +0900

    Free_chat 5.29

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 
 
 
-
+//새로운 유저 접속
 io.sockets.on('connection', function(socket) {
   socket.on('newuser', function(name) {
     console.log(name + ' login');
@@ -20,14 +20,14 @@ io.sockets.on('connection', function(socket) {
     io.sockets.emit('update', {type: 'connect', name: 'SERVER', message: name + '님이 접속'});
   });
 
-
+//채팅메시지
   socket.on('message', function(data) {
     data.name = socket.name;
     console.log(data);
     socket.broadcast.emit('update', data);
   })
 
-
+//접속종료
   socket.on('disconnect', function() {
     console.log(socket.name + 'logout');
     socket.broadcast.emit('update', {type: 'disconnect', name: 'SERVER', message: socket.name + '님이 나감'});
